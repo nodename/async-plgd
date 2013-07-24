617d4964ccbd071e16d2574e65600c540318e23b
Alan Shaw <alan@nodename.com>
2013-07-24 13:51:54 -0700

    implement services with a pair of channels!

--- src/hoare/subroutines.clj
+++ src/hoare/subroutines.clj
@@ -4,30 +4,32 @@
 ;; 4.1 FUNCTION: DIVISION WITH REMAINDER
 
 (defn divider []
-  (let [c (chan)]
+  (let [in (chan)
+        out (chan)]
     (go
       (while true
-        (let [{:keys [x y]} (<! c)]
+        (let [{:keys [x y]} (<! in)]
           (if (= y 0)
-            (>! c {:error :divbyzero})
+            (>! out {:error :divbyzero})
             (loop [r x
                    q 0]
               (if (>= r y)
                 (recur (- r y) (inc q))
-                (>! c {:quotient q :remainder r})))))))
-    c))
+                (>! out {:quotient q :remainder r})))))))
+    [in out]))
 
 (defn test-divider []
-  (let [divider (divider)]
+  (let [[d-in d-out] (divider)]
     (go
       (dotimes [_ 20]
         (let [x (int (rand 100))
               y (int (rand 25))]
-          (>! divider {:x x :y y})
-          (let [{:keys [quotient remainder error]} (<! divider)]
+          (>! d-in {:x x :y y})
+          (let [{:keys [quotient remainder error]} (<! d-out)]
             (println x "/" y ":" (if-not (nil? error)
                                    (str "Error " error)
-                                   (str "quotient: " quotient " remainder: " remainder)))))))))
+                                   (str "quotient: " quotient " remainder: " remainder))))))))
+  nil)
 
 ;; 4.2 RECURSION: FACTORIAL
 
@@ -36,53 +38,83 @@
 ...
 Note: This unrealistic example introduces the technique of the 'iterative list of processes'
 which will be used to a better effect in later examples."
-  (let [c (chan)]
+  (let [in (chan)
+        out (chan)]
     (go
       (while true
-        (let [n (<! c)]
+        (let [n (<! in)]
           (if (> n 1)
-            (let [child (factorializer)]
-              (>! child (- n 1))
-              (>! c (* n (<! child))))
-            (>! c 1)))))
-  c))
+            (let [[child-in child-out] (factorializer)]
+              (>! child-in (- n 1))
+              (>! out (* n (<! child-out))))
+            (>! out 1)))))
+  [in out]))
 
-;; Here's your decorator pattern for a channel:
 (defn named-fact [name]
   "Wrap factorializer in a transformer that produces a string about the factorial"
-  (let [c (chan)
-        fact (factorializer)]
+  (let [in (chan)
+        out (chan)
+        [f-in f-out] (factorializer)]
     (go
       (while true
-        (let [n (<! c)]
-          (>! fact n)
-          (>! c (str name " says: factorial of " n " is " (<! fact))))))
-    c))
+        (let [n (<! in)]
+          (>! f-in n)
+          (>! out (str name " says: factorial of " n " is " (<! f-out))))))
+    [in out]))
     
 (defn run-factorializer [name]
-  "Print out 20 factorials"
-  (let [factorializer (named-fact name)]
+  "Print out 10 factorials"
+  (let [[f-in f-out] (named-fact name)]
     (go
-      (dotimes [_ 20]
+      (dotimes [i 10]
         (let [n (int (rand 20))]
-          (>! factorializer n)
-          (println (<! factorializer))))))
+          (>! f-in n)
+          (println i (<! f-out))))))
   nil)
 
 (defn test-factorializer []
-  "Print out twenty factorials for a and twenty for b concurrently"
+  "Print out ten factorials for a and ten for b concurrently"
   (run-factorializer 'a)
   (run-factorializer 'b))
+
+;; Well, that works, but you may have seen a and b stepping on each other a bit at the terminal.
+
+(defn fan-in
+  "Merge input channels into a single channel.
+Whenever either input has something ready, send it on."
+  ([ins] (fan-in (chan) ins))
+  ([c ins]
+    (go (while true
+          (let [[x] (alts! ins)]
+            (>! c x))))
+    c))
+
+(defn test-merged-factorial []
+  (let [[a-in a-out] (named-fact 'a)
+        [b-in b-out] (named-fact 'b)
+        c (fan-in [a-out b-out])]
+    
+    (go (while true
+          (println (<! c))))
+    
+    (go (dotimes [i 10]
+          (>! a-in i)))
+    
+    (go (dotimes [i 10]
+          (>! b-in i))))
+  
+  nil)
 
 ;; 4.3 DATA REPRESENTATION: SMALL SET OF INTEGERS
 ;; 4.4 SCANNING A SET
 
 (defn myset []
   "Implement a set as a process using only array-like methods"
-  (let [c (chan)]
+  (let [in (chan)
+        out (chan)]
     (go
       (loop [content []]
-        (let [[command n] (<! c)]
+        (let [[command n] (<! in)]
           (println command n)
           (condp = command
             ;; :has? n - reply true if n is in the set, false otherwise
@@ -90,7 +122,7 @@ which will be used to a better effect in later examples."
                    (loop [i 0]
                      (if (and (< i (count content)) (not= (nth content i) n))
                        (recur (inc i))
-                       (>! c (< i (count content)))))
+                       (>! out (< i (count content)))))
                    (recur content))
             ;; :insert n - insert n into the set
             :insert (let [found (loop [i 0]
@@ -103,41 +135,37 @@ which will be used to a better effect in later examples."
                     (loop [i 0]
                       (if (< i (count content))
                         (do
-                          (>! c [:next (nth content i)])
+                          (>! out [:next (nth content i)])
                           (recur (inc i)))
-                        (>! c [:noneleft])))
+                        (>! out [:noneleft])))
                     (recur content))))))
-    c))
+    [in out]))
 
-;; rather than making this a function with its own go block
-;; and trying to sync it with the caller,
-;; I've made it a macro so its code will just
-;; drop into the caller verbatim
-(defmacro scan-set [f myset]
+(defmacro scan-set [f set-in set-out]
   "Invoke f on each element of the set"
   `(do
-     (>! ~myset [:scan])
+     (>! ~set-in [:scan])
      (loop []
-       (let [[response value] (<! ~myset)]
+       (let [[response value] (<! ~set-out)]
          (when (= response :next)
            (do
              (~f value)
              (recur)))))))
 
 (defn test-set []
-  (let [myset (myset)]
+  (let [[set-in set-out :as myset] (myset)]
     (go
-      (>! myset [:has? 4])
-      (println (<! myset))
-      (>! myset [:insert 4])
-      (>! myset [:insert 4])
-      (>! myset [:has? 4])
-      (println (<! myset))
-      (scan-set println myset)
-      (>! myset [:insert 3])
-      (>! myset [:has? 3])
-      (println (<! myset))
-      (scan-set println myset)))
+      (>! set-in [:has? 4])
+      (println (<! set-out))
+      (>! set-in [:insert 4])
+      (>! set-in [:insert 4])
+      (>! set-in [:has? 4])
+      (println (<! set-out))
+      (scan-set println set-in set-out)
+      (>! set-in [:insert 3])
+      (>! set-in [:has? 3])
+      (println (<! set-out))
+      (scan-set println set-in set-out)))
   nil)
 
 ;; 4.5 RECURSIVE DATA REPRESENTATION: SMALL SET OF INTEGERS
