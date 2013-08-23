b7e9a90070f809e95adc05d1790d7289803ef2c2
Alan Shaw <alan@nodename.com>
2013-08-22 23:18:20 -0700

    added print-all<!, using fipp

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [core.async "0.1.0-SNAPSHOT"]])
+                 [core.async "0.1.0-SNAPSHOT"]
+                 [fipp "0.4.0"]])
--- src/utils/helpers.clj
+++ src/utils/helpers.clj
@@ -1,11 +1,15 @@
 (ns utils.helpers
-  (:require [clojure.core.async :refer :all]
-            [clojure.pprint :refer [pprint]]))
+  (:require [clojure.core.async :refer [<! go]]
+             [fipp.edn :refer [pprint]]))
 
 (defn pprinter []
   (let [in (chan)]
     (go (while true
-          (pprint (<! in))))
+          (let [value (<! in)]
+            (if (nil? value)
+              (println "")
+              (do
+                (pprint value))))))
     in))
 
 (defn sink []
@@ -13,3 +17,15 @@
     (go (while true
           (<! in)))
     in))
+
+(defn print-all<!
+  "Dump everything from channel to stdout until channel closes, then print a newline"
+  [channel]
+  (go
+    (while true
+      (let [value (<! channel)]
+        (if (nil? value)
+          (println "")
+          (do
+            (.print System/out (str value \newline \newline)) ; This is apparently different from (print value), which doesn't flush everything until the user hits return!
+            (println "")))))))
